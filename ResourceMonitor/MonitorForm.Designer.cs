08d7ea0a5a9f949466905808e967ba453c4b44ac
georgel2020 <lingeorge2020@outlook.com>
2023-02-11 16:01:51 +0800

    Add project files.

---
+++ ResourceMonitor/MonitorForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ResourceMonitor
+{
+    partial class MonitorForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MemoryLabel = new System.Windows.Forms.Label();
+            this.CpuLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MemoryLabel
+            // 
+            this.MemoryLabel.AutoSize = true;
+            this.MemoryLabel.Font = new System.Drawing.Font("Cascadia Code", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MemoryLabel.Location = new System.Drawing.Point(13, 13);
+            this.MemoryLabel.Name = "MemoryLabel";
+            this.MemoryLabel.Size = new System.Drawing.Size(81, 20);
+            this.MemoryLabel.TabIndex = 0;
+            this.MemoryLabel.Text = "Memory: ";
+            this.MemoryLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MonitorForm_MouseDown);
+            // 
+            // CpuLabel
+            // 
+            this.CpuLabel.AutoSize = true;
+            this.CpuLabel.Font = new System.Drawing.Font("Cascadia Code", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CpuLabel.Location = new System.Drawing.Point(13, 42);
+            this.CpuLabel.Name = "CpuLabel";
+            this.CpuLabel.Size = new System.Drawing.Size(54, 20);
+            this.CpuLabel.TabIndex = 1;
+            this.CpuLabel.Text = "CPU: ";
+            this.CpuLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MonitorForm_MouseDown);
+            // 
+            // MonitorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(167, 74);
+            this.Controls.Add(this.CpuLabel);
+            this.Controls.Add(this.MemoryLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MonitorForm";
+            this.Text = "MonitorForm";
+            this.TopMost = true;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MonitorForm_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MemoryLabel;
+        private System.Windows.Forms.Label CpuLabel;
+    }
+}
+
---
+++ ResourceMonitor/MonitorForm.cs
@@ -0,0 +1,109 @@
+﻿using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System;
+using System.IO;
+
+namespace ResourceMonitor
+{
+    public partial class MonitorForm : Form
+    {
+        public MonitorForm()
+        {
+            InitializeComponent();
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            ReadConfig();
+
+            trd = new Thread(new ThreadStart(TrdTask));
+            trd.Start();
+
+            rect = new Rectangle(0, 0, this.Width, this.Height);
+            formPath = GetRoundedRectPath(rect, 16);
+            Region = new Region(formPath);
+
+            StartPosition = FormStartPosition.Manual;
+            Location = new Point(Screen.PrimaryScreen.Bounds.Width - Width - 10, 10);
+
+            if (clickThrough)
+            {
+                SetWindowLong(Handle, -20, 0x20 | 0x80000);
+                SetLayeredWindowAttributes(Handle, 0, 100, 0);
+            }
+
+            if (!alwaysOnTop)
+            {
+                TopMost = false;
+            }
+        }
+
+        private void MonitorForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x0112, 0xF012, 0);
+        }
+
+        private void ReadConfig()
+        {
+            using (StreamReader reader = new StreamReader("ResourceMonitor.txt"))
+            {
+                alwaysOnTop = reader.ReadLine() == "true";
+                clickThrough = reader.ReadLine() == "true";
+            }
+        }
+
+        private void TrdTask()
+        {
+            while (true)
+            {
+                MemoryLabel.Text = "Memory: " + memoryUsage.NextValue().ToString("0.00") + "%";
+                CpuLabel.Text = "CPU: " + cpuUsage.NextValue().ToString("0.00") + "%";
+                Thread.Sleep(1000);
+            }
+        }
+
+        private GraphicsPath GetRoundedRectPath(Rectangle rect, int radius)
+        {
+            int diameter = radius;
+            Rectangle arcRect = new Rectangle(rect.Location, new Size(diameter, diameter));
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(arcRect, 180, 90);
+            arcRect.X = rect.Right - diameter;
+            path.AddArc(arcRect, 270, 90);
+            arcRect.Y = rect.Bottom - diameter;
+            path.AddArc(arcRect, 0, 90);
+            arcRect.X = rect.Left;
+            path.AddArc(arcRect, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        Thread trd;
+        GraphicsPath formPath;
+        Rectangle rect;
+
+        bool alwaysOnTop, clickThrough;
+
+        public PerformanceCounter memoryUsage = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+        public PerformanceCounter cpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+        [DllImport("user32", EntryPoint = "SetWindowLong")]
+        private static extern uint SetWindowLong(IntPtr hwnd, int nIndex, uint dwNewLong);
+
+        [DllImport("user32", EntryPoint = "GetWindowLong")]
+        private static extern uint GetWindowLong(IntPtr hwnd, int nIndex);
+
+        [DllImport("user32", EntryPoint = "SetLayeredWindowAttributes")]
+        private static extern int SetLayeredWindowAttributes(IntPtr hwnd, int crKey, int bAlpha, int dwFlags);
+    }
+}
